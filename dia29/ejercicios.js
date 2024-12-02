731c10adfb663a1580a37f284ecf9e7827df80da
MarioElguera <marioelguera63@alumnos.cei.es>
2024-12-02 19:04:02 +0100

    refactor code

--- dia29/ejercicios.js
+++ dia29/ejercicios.js
@@ -2,7 +2,7 @@
 function mostrarPares() {
     for (let i = 0; i <= 100; i++) {
         if (i % 2 == 0) {
-            // console.log("Número par =>" + i);
+            console.log("Número par =>" + i);
         }
     }
 }
